6246baca93557b0503b9168096b53d60b50e32d5
Prachi <ihcarpshukla1906@gmail.com>
2019-08-31 12:17:06 +0530

    first commit

---
+++ test.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+int main()
+{
+    int a = 5;
+    cout<<"enter the name ";
+    cout<<"enter the age";
+    cout<<"My name is prachi";
+    
+    cout<<a<<endl;
+
+    return 0;
+    
+}//step 1:
